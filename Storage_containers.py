5db309e76fa185c138cfa2e79b49e327693a29da
Anthony Phan <aphan5@students.kennesaw.edu>
2023-09-15 02:12:22 -0400

    Created baseline for the project

--- Storage_containers.py
+++ Storage_containers.py
@@ -1,1 +1,61 @@
 # TODO Add storage containers
+class Storage_containers:
+    def __init__ (self, name, length, height, width, weight_capacity):
+        self.name = name
+        self.length = length
+        self.height = height
+        self.width = width
+        self.weight_capacity = weight_capacity
+
+    def __init__ (self, name, length, height, width):
+        self.name = name
+        self.length = length
+        self.height = height
+        self.width = width
+
+    def box_truck():
+        name = 'Box Truck'
+        length = 24
+        height = 8
+        width = 8
+        cubic_area = length * height * width
+        weight_capacity = 26000
+
+    def ship_containers_10ft():
+        name = 'Shipping Container 10ft'
+        length = 10
+        height = 8.6
+        width = 8
+        cubic_area = length * height * width
+        weight_capacity = 14800
+
+    def ship_containers_20ft():
+        name = 'Shipping Container 20ft'
+        length = 20
+        height = 8.6
+        width = 8
+        cubic_area = length * height * width
+        weight_capacity = 29600
+    
+    def ship_containers_40ft():
+        name = 'Shipping Container 40ft'
+        length = 40
+        height = 8.6
+        width = 8
+        cubic_area = length * height * width
+        weight_capacity = 59200
+    
+    def storage_unit():
+        name = 'Storage Unit'
+        length = 20
+        height = 8
+        width = 10
+        cubic_area = length * height * width
+    
+    def pallets():
+        name = 'Pallets'
+        length = 4
+        height = 5
+        width = 3.4
+        cubic_area = length * height * width
+        weight_capacity = 4600
---
+++ max_storage.py
@@ -0,0 +1,45 @@
+'''
+The goal for our plan
+is given a cubic area, choose the best container for those shipment
+then the algorithm will place the items in the containers that will optimize the space
+the output will be in a form for users to load up the items. 
+
+'''
+class max_storage:
+    def item_list(self, item_list):
+
+
+    # Sort the list of items by cubic area in descending order and return the sorted list
+    def sort_items(item_list):
+        sorted_items = []
+        sort_check = 0
+        change_check = False
+        item_count = item_list.length 
+        iteration = 0
+        # Sort this by cubic area
+        while change_check == False and iteration == item_count:
+
+            if(sorted_items[iteration] > sort_check):
+                temp = sort_check
+                sort_check = sorted_items[iteration]
+                sorted_items[iteration] = temp
+                change_check = True
+                iteration += 1 
+
+        return sorted_items
+    
+    def sort_weight(item_list):
+        sorted_items = []
+        heavy_item = 0
+        item_count = 0
+        iteration = 0
+
+        
+    # Given the list of items and storage containers, verify you can fit each items into the containers
+    def tetris(item_list, storage_containers):
+        length = storage_containers.length
+        height = storage_containers.height
+        width = storage_containers.width
+        current_capacity_length = length
+        current_capacity_height = height
+        current_capacity_width = width
